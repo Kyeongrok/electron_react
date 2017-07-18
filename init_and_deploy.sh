9936dfe4170186ad3a09704478ef6b662c7bafec
root <root@ip-172-31-26-188.us-east-2.compute.internal>
2017-07-18 21:43:45 +0900

    modify init_add-

--- init_and_deploy.sh
+++ init_and_deploy.sh
@@ -1,10 +1,17 @@
 cd /var/www/electron_react/
 git pull
+npm update
 npm run build
 
 echo "####### rsync #######"
 rsync -ar /var/www/electron_react/build/ /usr/share/nginx/html
 
 echo "######## restart server #######"
-forever restart server
+
+cd /var/www/aprilskin_api/
+git pull
+npm update
+forever restart --minUptime 1000 --spinSleepTime 1000 ./bin/www
+
+#forever restart server
 
